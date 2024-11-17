22d3a9f3dc0acad01a73359daeb93846ce7068d5
HARIPRASAD27102003 <27102003hari@gmail.com>
2024-11-17 13:04:39 +0530

    change

--- car_management/cars/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-# cars/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_suffix')
-def add_suffix(value, suffix):
-    """Add a suffix to the value."""
-    return str(value) + str(suffix)
